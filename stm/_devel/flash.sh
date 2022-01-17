fd65ec83c1f790fbd31698a2b62b045e1fe71741
Cameron Bentley <cameron@bentley.sh>
2022-01-16 17:08:46 -0700

    testing env setup

--- stm/_devel/flash.sh
+++ stm/_devel/flash.sh
@@ -3,6 +3,6 @@ read -p 'tagname: ' tagvar
 
 set -e
 
-DOCKER_BUILDKIT=1 docker build --build-arg tagvar=$tagvar -t registry.bentley.sh/stm/flasher:$tagvar ./stm/flasher
+DOCKER_BUILDKIT=1 docker build --build-arg tagvar=$tagvar -t registry.bentley.sh/stm/flasher:$tagvar ../flasher
 docker push registry.bentley.sh/stm/flasher:$tagvar
 docker run --privileged registry.bentley.sh/stm/flasher:$tagvar
